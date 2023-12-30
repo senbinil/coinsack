1ac2b4b2584d0bdd337dfd9ce71221e1974406ea
Binil Sen B <senoffical111@gmail.com>
2023-12-30 10:38:26 +0530

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coinsack
+
+Coinsack is cryptocurrency listing portal providing latest coin price.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h1" align="center">
+        Coinsack
+      </Typography>
+    </>
+  );
+}
+
+export default App;
